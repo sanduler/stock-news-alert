a34124d0d82024cddb8930d1e4c4cfd9b5013807
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-02-21 13:04:20 -0800

    added all the API keys and endpoints, created blank dictionaries for parameters.

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+# Name: Ruben Sanduleac
+# Date: 02/21/22
+# Description: Stock News Alert notifies the user every morning about the
+#              most relevant news info based on increase or decrease in stock.
+#              The program uses Stock API from Alpha Vintage, News API for
+#              newsapi.org, and twilio API to send text.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import os
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = os.environ["STOCK_API"]
+NEWS_API_KEY = os.environ["NEWS_API"]
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+FROM_PHONE = os.environ["FROM_TEXT"]
+TO_PHONE = os.environ["TO_TEXT"]
+
+# pull the needed parameters from stock
+parameters_stock = {}
+
+# get a response from Stocks API
+response_stock = requests.get(url=STOCK_ENDPOINT, params=parameters_stock)
+response_stock.raise_for_status()
+stock_data = response_stock.json()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# pull the needed parameters from news
+parameters_news = {}
 
+# get a response from Stocks API
+response_news = requests.get(url=NEWS_ENDPOINT, params=parameters_news)
+response_news.raise_for_status()
+news_data = response_news.json()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
