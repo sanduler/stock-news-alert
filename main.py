c7712230a0d58fc8ed9fe713dbe34155d7faef28
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-02-21 14:07:08 -0800

    finished implementing the pull for the stock data

--- main.py
+++ main.py
@@ -4,15 +4,13 @@
 #              most relevant news info based on increase or decrease in stock.
 #              The program uses Stock API from Alpha Vintage, News API for
 #              newsapi.org, and twilio API to send text.
-
 import requests
 import os
 from twilio.rest import Client
-from twilio.rest.api.v2010.account.usage.record import yesterday
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
+TARGET_DIFFERENCE = 5
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
@@ -38,11 +36,22 @@ stock_data = response_stock.json()["Time Series (Daily)"]
 # print(stock_data)
 
 stock_data_list = [value for (key, value) in stock_data.items()]
-yesterday_closing = stock_data_list[0]["4. close"]
-day_before_yesterday_closing = stock_data_list[1]["4. close"]
-print(yesterday_closing)
-print(day_before_yesterday_closing)
+yest_closing = float(stock_data_list[0]["4. close"])
+day_before_yest_closing = float(stock_data_list[1]["4. close"])
+# print(yest_closing)
+# print(day_before_yest_closing)
 # print(stock_data_list)
+
+difference_closing = abs(yest_closing - day_before_yest_closing)
+# print(difference_closing)
+
+percent_difference = round((difference_closing / day_before_yest_closing) * 100, 2)
+# print(percent_difference)
+
+if percent_difference > TARGET_DIFFERENCE:
+    print("Get News!")
+else:
+    print(percent_difference)
 
 # pull the needed parameters from news
 parameters_news = {}
